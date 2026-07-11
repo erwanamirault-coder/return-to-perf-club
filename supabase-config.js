09ca500809990ed72c72cd8a5fe4be723d850072
erwanamirault-coder <erwan.amirault@gmail.com>
2026-07-11 15:58:57 +0200

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://uuhjjfnbuwqvktakegmk.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_DWonhP11UEzLrA93eiktgQ_pKDFEoIx";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
